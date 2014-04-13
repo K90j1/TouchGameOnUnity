9f4abc761e152c441c9cf01eea5f6a868d33c92f
bookair17 <kyoji-exterior@goo.jp>
2014-04-13 21:04:59 +0900

    first commit

---
+++ Assets/Scripts/player2.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+function Update () {
+  if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+    var hit: RaycastHit;
+    var ray: Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	Debug.Log(ray);
+	Debug.Log(hit);
+    if (Physics.Raycast(ray, hit) && hit.transform.name=="Map"){
+      // hit.point contains the point where the ray hits the
+      // object named "Map"
+    }
+  }
+}
